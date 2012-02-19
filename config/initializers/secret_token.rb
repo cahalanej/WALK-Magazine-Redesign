5e122d7e8586ec2873986304d0afa0b0da45d97d
Jason Mow <jason.mow@gmail.com>
2012-02-19 09:06:24 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WALKMagazine::Application.config.secret_token = '29ca654dd015f72e4cedc62b29d7e2318b6b11109908a4c17e49cf1370abd85aa086fa4fa6677c980485bd9a334d27cc97af4f7ae606b8ae27136a06c5a578a6'
